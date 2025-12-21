38d7875622ebbf85bdd16a96c883b55f8a67d927
Tim <vuluongquy2003@gmail.com>
2025-12-20 23:34:10 -0500

    DEBUG fixed

--- run_dev.py
+++ run_dev.py
@@ -1,10 +1,6 @@
-import os
+# run_dev.py
 from app import create_app
 
 if __name__ == "__main__":
-    # Local defaults
-    os.environ.setdefault("FLASK_ENV", "development")
-    os.environ.setdefault("FLASK_DEBUG", "1")
-
     app = create_app()
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000)
